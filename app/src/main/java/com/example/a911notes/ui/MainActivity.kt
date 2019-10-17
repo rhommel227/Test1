dd7429c00492a81c14f8316811b7279cb8507f64
ryanh <rwhstuff@gmail.com>
2019-10-16 20:30:32 -0700

    Git Test

--- app/src/main/java/com/example/a911notes/ui/MainActivity.kt
+++ app/src/main/java/com/example/a911notes/ui/MainActivity.kt
@@ -14,7 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private var imageCounter: Long = 0
     private var userName: String = ""
-    private fun getStore() = getPreferences(Context.MODE_PRIVATE)
+
 
     private lateinit var countViewModel: CountViewModel
 
@@ -24,18 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
         countViewModel.getUserCount( intent.extras?.get("username").toString()).observe(this,
-            androidx.lifecycle.Observer {updateCounter(it)})
+            androidx.lifecycle.Observer {
+                updateCounter(it)})
 
         userName = intent.extras?.get("username").toString()
 
-        if (savedInstanceState != null) {
-           imageCounter = savedInstanceState.getLong(IMAGE_COUNTER_KEY, 0)
-        } else if (getStore().contains(userName)) {
-            imageCounter = getStore().getLong(userName, 0)
-        }
 
         if (imageCounter > 0) {
-            myCounter.text = imageCounter.toString()
+           updateCounter(imageCounter)
         }
 
         myButton.setOnClickListener {
@@ -55,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
 
         myEnableButton.setOnClickListener{
-            imageCounter= imageCounter - 10
+            countViewModel.setUserCount(intent.extras?.get("username").toString(), imageCounter - 10)
             button2.isVisible = true
             myEnableButton.isVisible = false
 
@@ -63,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         button2.setOnClickListener{
-            imageCounter = imageCounter +2
+            countViewModel.setUserCount(intent.extras?.get("username").toString(), imageCounter + 2)
             myImage.rotate90()
             myImage.rotate90()
 
@@ -79,20 +75,7 @@ class MainActivity : AppCompatActivity() {
         myCounter.text = imageCounter.toString()
     }
 
-    override fun onPause() {
-        super.onPause()
-        getStore().edit().putLong(userName, imageCounter).apply()
-    }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        outState?.run {
-            putLong(IMAGE_COUNTER_KEY, imageCounter)
-        }
 
-        super.onSaveInstanceState(outState)
-    }
 
-    companion object {
-        private const val IMAGE_COUNTER_KEY = "imageCounterKey"
-    }
 }
